4b34a8dd6fcf8de07f2e9c082a91a1b044224bd3
rawshad <rawshad@outlook.com>
2023-01-16 18:25:53 +0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MoviesHomeComponent},
+  {path: 'addMovie', component: AddMoviesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    MoviesHomeComponent,
+    AddMoviesComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
